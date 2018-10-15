3148dc3e36816867156957234e1700627c710410
kyhorne <rcac98@gmail.com>
2018-10-14 20:59:09 -0400

    Updated buyPlant method and TODO list

--- PvZ/src/PvZModel.java
+++ PvZ/src/PvZModel.java
@@ -4,25 +4,40 @@ import java.util.Scanner;
 
 public class PvZModel {
 
+	/**
+	 * Spawned entities. 
+	 */
 	private ArrayList<Entity> entities;
   	
+	/**
+	 * Game balance.
+	 */
 	private int sunpoints;
 	
+	/**
+	 * Current game iteration.
+	 */
 	public int turnCounter;
-	
+
 	private Board gameBoard;
 
 	private Scanner reader;
 	
+	/**
+	 * The period which sun points are automatically rewarded to balance.
+	 */
+	public static final int PAYMENT_PERIOD = 4;
+	
 	public PvZModel() {
 		 entities = new ArrayList<Entity>();
-		 sunpoints = 50;
+		 sunpoints = 50; // Starting balance
 		 gameBoard = new GameBoard();
 		 turnCounter = 0;
 	}	
 	
-	private Point getInput() {
-		System.out.println("Enter a location:");
+	private Point getLocation(String entity) {
+		System.out.println("Enter a location to spawn" + entity + ": ");
+		// Read from standard out 
 		reader = new Scanner(System.in);
 		String input = reader.next();
 		Point location = new Point(Character.getNumericValue(input.charAt(1)), input.charAt(0) - 65);
@@ -36,85 +51,66 @@ public class PvZModel {
 		return false;
 	}
 	
-	@SuppressWarnings("unused")
-	private void buy() {
-		
-		System.out.println("Enter the plant you would like to purchase:");
-		reader = new Scanner(System.in);
-		String input = reader.next().toLowerCase();	
-		
-		switch(input) {
-		
-		case "sunflower":
-			
-			if(sunpoints >= Sunflower.COST && Sunflower.RECHARGE_TIME == 0) {
-				
-				sunpoints -= Sunflower.COST;
-				System.out.println("You just purchased a Sunflower!");
-				entities.add(new Sunflower(getInput()));
-			}
-			
-			else {
-				
-				System.out.println("You do not have enough sunpoints to purchase this!");
-			}
-			
-			
-			break;
-			
-		case "peashooter":
-			
-			if(sunpoints > PeaShooter.COST && PeaShooter.RECHARGE_TIME == 0) {
-				
-
-				sunpoints -= PeaShooter.COST;
-				System.out.println("You just purchased a PeaShooter!");
-				entities.add(new PeaShooter(getInput()));			
-			}
-			
-			else {
-				
-				System.out.println("You do not have enough sunpoints to purchase this!");
-				
-			}
-			
-			break;
-			
-		default: 
-			
-			System.out.println("Next turn!");
-			break;
-		
+	private void buyPlant() {
+		// TODO: Fix tight coupling between plant name and cost
+		// TODO: Maybe make this method functional 
+		// TODO: Decide whether a player can buy multiple plants each game iteration
+		// Check if user has sufficient balance to make purchase
+		System.out.println("Sunpoints: " + sunpoints);
+		if (sunpoints < Sunflower.COST && sunpoints < PeaShooter.COST) {
+			System.out.println("Insuffient sunpoint balance.");
+			return;
 		}
-		
-		
-		
+		// Print available items to purchase 
+		System.out.println("Items available for purcahse:");
+		if (sunpoints < Sunflower.COST) {
+			System.out.println("< Sunflower : " + Sunflower.COST + " >");
+		}
+		if (sunpoints < PeaShooter.COST) {
+			System.out.println("< PeaShooter : " + Sunflower.COST + " >");
+		} 
+		// Read from standard out
+		reader = new Scanner(System.in);
+		String input = reader.next().toUpperCase();
+		switch(input) {
+			case "SUNFLOWER":	
+				if (sunpoints < Sunflower.COST) {
+					sunpoints -= Sunflower.COST;
+					entities.add(new Sunflower(getLocation("Sunflower")));	
+				}
+				break;
+			case "PEASHOOTER":
+				if (sunpoints < PeaShooter.COST) {
+					sunpoints -= PeaShooter.COST;
+					entities.add(new PeaShooter(getLocation("Peashooter")));
+				} 									
+				break;
+		}
 	}
 	
 	private void gameLoop() {
-		// TODO: CHECK IF USER HAS BEAT ROUND 
-		// TODO: NEED TO SPAWN ZOMBIES
-		// TODO: NEED TO ASK USER WHAT THE WANT TO BUY
-		// TODO: NEED PAYMENT SYSTEM
-		// TODO: LOCATION BUG
+		// TODO: Fix location bug
+		// TODO: Spawn zombies 
+		// TODO: Spawn bullets 
+		// TODO: Instances of moveable need to update position
+		// TODO: Check if the round is over
 		gameBoard.print();
 		while (!isGameOver()) {
 			gameBoard.clear();
-
-			System.out.println("Sunpoints: " + sunpoints);
-			buy();
+			buyPlant();
 			
 			for(Entity e : entities) {
-				if (e instanceof Moveable) ((Moveable) e).updatePosition();
 				gameBoard.addEntity(e.getX(), e.getY(), e.getLabel());
-				
-				
-				
 			}
+			
 			turnCounter++;
 			gameBoard.print();
+			
+			if (turnCounter % PAYMENT_PERIOD == 0) {
+				sunpoints += 25;
+			}
 		}
-		// TODO: NEED TO CLOSE READER
+		// TODO: Need to close reader
 	}
 	
 	public static void main(String args[]) {
