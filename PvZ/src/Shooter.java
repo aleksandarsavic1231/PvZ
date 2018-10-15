58e9e0bc52a81d7926fef66625e5883d8b36c64f
kyhorne <rcac98@gmail.com>
2018-10-14 21:52:08 -0400

    Removed commented code in Shooter

--- PvZ/src/Shooter.java
+++ PvZ/src/Shooter.java
@@ -4,7 +4,6 @@ public abstract class Shooter extends Alive {
 	
 	private int fireRate;
 
-	
 	public Shooter(Point position, char label, int health) {
 		super(position, label, health);
 		this.fireRate = 0;	
@@ -13,11 +12,7 @@ public abstract class Shooter extends Alive {
 	public int setFireRate(int fireRate) {
 		return this.fireRate = fireRate;
 	}
-	
-//	public int setPlantCooldown(int plantCooldown) {
-//		return this.plantCooldown = plantCooldown;
-//	}
-	
+
 	public boolean canShoot() {
 		if (fireRate == 0) {
 			resetFireRate();
@@ -27,14 +22,6 @@ public abstract class Shooter extends Alive {
 		return false;
 	}
 	
-//	public boolean canPlant() {
-//		if (plantCooldown == 0) {
-//			resetPlantCooldown();
-//			return true;
-//		}
-//		this.plantCooldown--;
-//		return false;
-//	}
 	abstract public void resetFireRate();
 
 }
