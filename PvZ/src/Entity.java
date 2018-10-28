a0fb6db70eda323cbcb59b6a4cae28ffcc541753
Kyle Horne <rcac98@gmail.com>
2018-10-28 18:24:25 -0400

    Update Entity.java

--- PvZ/src/Entity.java
+++ PvZ/src/Entity.java
@@ -1,27 +1,50 @@
 import java.awt.Point;
 
-/** Entity
+/**
+ * Entity is a location of a entity in the game.
  * 
- * Entity applies to all entities in the game
- * It keeps track of the location of all entities
+ * @author kylehorne
+ * @version 28 Oct 18
  */
-
 public abstract class Entity {
 	
+	/**
+	 * The location of the Entity.
+	 */
 	private Point p;
 	
+	/**
+	 * Constructor.
+	 * 
+	 * @param p The location of the Entity.
+	 */
 	public Entity(Point p) {
 		this.p = p;
 	}
 
+	/**
+	 * Get the x coordinate of the Entity.
+	 * 
+	 * @return int The x coordinate of the Entity.
+	 */
 	public int getX() {
 		return p.x;
 	}
 	
+	/**
+	 * Get the y coordinate of the Entity.
+	 * 
+	 * @return int The y coordinate of the Entity.
+	 */
 	public int getY() {
 		return p.y;
 	}
 	
+	/**
+	 * Get the location of this Entity.
+	 * 
+	 * @return Point The location of this Entity.
+	 */
 	public Point getPoint() {
 		return p;
 	}
