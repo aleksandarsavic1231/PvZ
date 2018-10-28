57fdaf867d249c5b75c1e37afc0b5fc8b56df274
Kyle Horne <rcac98@gmail.com>
2018-10-28 18:23:20 -0400

    Update Board.java

--- PvZ/src/Board.java
+++ PvZ/src/Board.java
@@ -1,20 +1,43 @@
 import java.awt.Point;
 
-/** Board
+/**
+ * Board interface.
  * 
- * Board interface consists of methods clear, print, isValidLocation, addEntity, removeEntity
+ * @author kylehorne
+ * @version 28 Oct 18
  */
-
 public interface Board {
 		
+	/**
+	 * Clear board.
+	 */
 	public void clear();
 
+	/**
+	 * Print board.
+	 */
 	public void print();
 	
+	/**
+	 * Check whether a input String is a valid location on the board.
+	 * 
+	 * @param input The input location.
+	 * @return Point Point object of a valid input String.
+	 */
 	public Point isValidLocation(String input);
 			
+	/**
+	 * Add Entity object to board.
+	 * 
+	 * @param e Entity object.
+	 */
 	public void addEntity(Entity e);
 	
+	/**
+	 * Remove Entity from board.
+	 * 
+	 * @param e Entity object.
+	 */
 	public void removeEntity(Entity e);
 
 }
