6a2150517ea4e98436640533e04fd1bb35cc8bd6
kyhorne <rcac98@gmail.com>
2018-10-28 17:18:31 -0400

    Added Javadoc comments for Alive class

--- PvZ/src/Alive.java
+++ PvZ/src/Alive.java
@@ -1,25 +1,44 @@
 import java.awt.Point;
 
-/** Alive
+/**
  * 
- * The Alive class only applies for entity that are still considered alive in the game.
- * It has the health attribute to keep track of each entities health
- *
+ * The Alive class only applies for Entities that have a health attribute.
+ * 
+ * @author kylehorne
+ * @version 28 Oct 18
  */
-
 public class Alive extends Entity {
 
+	/**
+	 * The health of this Entity.
+	 */
 	private int health;
 	
+	/**
+	 * Constructor.
+	 * 
+	 * @param position Location of entity.
+	 * @param health Health of entity.
+	 */
 	public Alive(Point position, int health) {
 		super(position);
 		this.health = health;
 	}
 	
+	/**
+	 * Get health of this Entity.
+	 * 
+	 * @return int Health of this Entity.
+	 */
 	public int getHealth() {
 		return this.health;
 	}
 	
+	/**
+	 * Set Entity health.
+	 * 
+	 * @param health New health of Entity.
+	 */
 	public void setHealth(int health) {
 		this.health -= health;
 	}
