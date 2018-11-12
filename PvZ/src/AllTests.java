88236564de23838a463a607c25e7c98960a2a2f8
Tashfiq Akhand <tashfiqakhand@gmail.com>
2018-11-12 15:34:46 -0500

    Create AllTests.java

---
+++ PvZ/src/AllTests.java
@@ -0,0 +1,28 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AliveTest.class, BulletTest.class, 
+	PeaShooterTest.class, SunflowerTest.class, ZombieTest.class })
+public class AllTests extends TestCase{
+	
+	public static void main(String[] args) { 
+		junit.textui.TestRunner.run(AllTests.class);
+	} 
+	
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for Plants Vs Zombies game"); 
+		suite.addTest(new TestSuite(AliveTest.class)); 	
+		suite.addTest(new TestSuite(BulletTest.class)); 
+		suite.addTest(new TestSuite(PeaShooterTest.class)); 
+		suite.addTest(new TestSuite(SunflowerTest.class)); 
+		suite.addTest(new TestSuite(ZombieTest.class)); 
+		return suite;
+	} 
+
+}
