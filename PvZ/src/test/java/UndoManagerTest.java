372c8abebe557376c723c8b96b4873b23516ecad
Kaj <Kaj@192.168.0.10>
2018-11-25 23:13:10 -0500

    Frame for UndoManagerTest

--- PvZ/src/test/java/UndoManagerTest.java
+++ PvZ/src/test/java/UndoManagerTest.java
@@ -1,5 +1,50 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class UndoManagerTest extends TestCase {
 
+	private UndoManager undoManager;
+	
+	@Before
+	public void setUp() throws Exception {
+		undoManager = new UndoManager();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		undoManager = null;
+	}
+	
+	@Test
+	public void testExecute() {
+		
+	}
+	
+	@Test
+	public void testUndo() {
+		
+	}
+	
+	@Test
+	public void TestRedo() {
+		
+	}
+	
+	@Test
+	public void TestClear() {
+		
+	}
+	
+	@Test
+	public void testIsUndoAvailable() {
+		
+	}
+	
+	@Test
+	public void testIsRedoAvailable() {
+		
+	}
 }
