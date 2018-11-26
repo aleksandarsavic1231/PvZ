e69a5fbf5a74e0d682b77ae86211a36dd5571a76
kyhorne <rcac98@gmail.com>
2018-11-25 23:31:59 -0500

    one more

---
+++ PvZ/src/test/java/ToggleCommandTest.java
@@ -0,0 +1,28 @@
+import java.awt.Point;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TogglePlantCommandTest extends TestCase {
+	
+	private Sunflower sunflower;
+
+	@Before
+	public void setUp() throws Exception {
+		sunflower = new Sunflower(new Point(0, 0));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		sunflower = null;
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
