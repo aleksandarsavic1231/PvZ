df419fea57234043df4978d746cc00129ff468b1
Kaj <Kaj@192.168.0.10>
2018-11-25 22:34:14 -0500

    jAvAdOc

--- PvZ/src/main/java/UndoManager.java
+++ PvZ/src/main/java/UndoManager.java
@@ -1,6 +1,11 @@
 import java.util.LinkedList;
 import java.util.Stack;
 
+/**Handles incoming commands from game events and places them on a stack so that a user can undo an action, or redo a previously performed action.
+ * 
+ * @author kylehorne
+ *@version Nov 25, 2018
+ */
 public class UndoManager {
 	
 	private LinkedList<Listener> listeners;
@@ -8,13 +13,19 @@ public class UndoManager {
  	private Stack<Undoable> undoStack;
  	
 	private Stack<Undoable> redoStack;
-	
+
+	/**
+	 * Constructor
+	 */
 	public UndoManager() {
 		listeners = new LinkedList<Listener>();
 		undoStack = new Stack<Undoable>();
 		redoStack = new Stack<Undoable>();
 	}
-	
+	/**
+	 * Passes a performed action onto a stack so that it may be accessed at a later time
+	 * @param command Action performed by the user
+	 */
 	public void execute(Undoable command) {
 		command.execute();
 		undoStack.push(command);
@@ -24,7 +35,9 @@ public class UndoManager {
 		redoStack.clear();
 		notifyListeners();
 	}
-	
+	/**
+	 * Pops the latest performed action off of the stack
+	 */
  	public void undo() {
 		if (undoStack.isEmpty()) return;
 		Undoable command = undoStack.pop();
@@ -32,7 +45,9 @@ public class UndoManager {
 		redoStack.push(command);
 		notifyListeners();
 	}
-	
+	/**
+	 * Re-does any amount of previously performed actions, until a user provides a new one
+	 */
 	public void redo() {
 		if (redoStack.isEmpty()) return;		
 		Undoable command = redoStack.pop();
@@ -40,17 +55,26 @@ public class UndoManager {
 		undoStack.push(command);
 		notifyListeners();
 	}
-	
+	/**
+	 * Empties the action stack
+	 */
 	public void clearUndoManager() {
 		redoStack.clear();
 		undoStack.clear();
 		notifyListeners();
 	}
 
+	/**
+	 * 
+	 * @return True when there are actions that can be undone
+	 */
  	public boolean isUndoAvailable() {
 		return !undoStack.isEmpty();
 	}
- 	
+ 	/**
+ 	 * 
+ 	 * @return True if there are actions in the redo stack
+ 	 */
 	public boolean isRedoAvailable() {
 		return !redoStack.isEmpty();
 	}
